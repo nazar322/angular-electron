42588c404f919f1485bbd7a3414651a99efb0771
Nazar <nazar.grynko@vitzo.com>
2024-09-07 20:19:37 +0300

    Spawn child and read std

--- app/ipc.js
+++ app/ipc.js
@@ -11,10 +11,28 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Ipc = void 0;
 const electron_1 = require("electron");
+const child_process_1 = require("child_process");
 class Ipc {
     static init() {
         electron_1.ipcMain.handle('get-media-info', (event, args) => __awaiter(this, void 0, void 0, function* () {
             console.log(args.url);
+            const ytdlp = (0, child_process_1.spawn)('C:\\Users\\Acer\\Downloads\\yt-dlp_win\\yt-dlp.exe', ['--write-info', '--dump-json', '--skip-download', args.url], { stdio: 'pipe' });
+            ytdlp.on('spawn', () => {
+                var _a, _b;
+                console.log(`yt-dlp (${ytdlp.pid}) spawned`);
+                (_a = ytdlp.stdout) === null || _a === void 0 ? void 0 : _a.on('data', (data) => {
+                    console.log(`stdout ${data}`);
+                });
+                (_b = ytdlp.stderr) === null || _b === void 0 ? void 0 : _b.on('data', (data) => {
+                    console.log(`stderr ${data}`);
+                });
+            });
+            ytdlp.on('exit', (code) => {
+                console.log(`yt-dlp has exited (${code})`);
+            });
+            ytdlp.on('error', (error) => {
+                console.log(`yt-dlp error: ${error}`);
+            });
             return 'Ok';
         }));
     }
--- app/ipc.ts
+++ app/ipc.ts
@@ -1,9 +1,34 @@
-import { ipcMain } from 'electron';
+import { ipcMain, utilityProcess } from 'electron';
+import { spawn } from 'child_process';
 
 export class Ipc {
     static init(): void {
         ipcMain.handle('get-media-info', async (event, args) => {
             console.log(args.url);
+
+            const ytdlp = spawn('C:\\Users\\Acer\\Downloads\\yt-dlp_win\\yt-dlp.exe', 
+                ['--write-info', '--dump-json', '--skip-download', args.url], {stdio: 'pipe'});
+
+            ytdlp.on('spawn', () => {
+                console.log(`yt-dlp (${ytdlp.pid}) spawned`);
+
+                ytdlp.stdout?.on('data', (data) => {
+                    console.log(`stdout ${data}`);
+                });
+
+                ytdlp.stderr?.on('data', (data) => {
+                    console.log(`stderr ${data}`);
+                });
+            });
+
+            ytdlp.on('exit', (code) => {
+                console.log(`yt-dlp has exited (${code})`);
+            });
+
+            ytdlp.on('error', (error) => {
+                console.log(`yt-dlp error: ${error}`);
+            });
+
             return 'Ok';
         });
     }
